50ab591cf8f08f3fb49ca9c0b392650a89e1cc9c
CodexNG <localhost@codexng.com>
2021-08-18 00:39:56 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Chukwudi < code > Onyeka < /code> -CodexNG < /
+        p > <
+        a className = "App-link"
+        href = "https://codexng.com"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Hire me! <
+        /a> < /
+        header > <
+        /div>
+    );
+}
+
+export default App;
